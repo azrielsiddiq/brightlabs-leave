d4a97d7327c3b7252f7bd80b07d35888dba8d19c
azrielsiddiq <azrielrsiddiq@gmail.com>
2026-06-13 19:11:09 +0700

    initialize leave system database structure

---
+++ database/migrations/2026_06_13_114724_create_leave_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('jenis_cuti', ['cuti_tahunan','cuti_sakit','cuti_penting']);
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->integer('jumlah_hari');
+            $table->text('alasan');
+            $table->enum('status', ['pending','approved','rejected','cancelled'])->default('pending');
+            $table->foreignId('approved_by')->nullable()->constrained('users')->nullOnDelete();
+            $table->timestamp('approved_at')->nullable();
+            $table->text('catatan_hrd')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+};
